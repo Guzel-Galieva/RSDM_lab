3eb8267493ec9692421ca464068cc36c4a6e52e3
guzel <guzel.vv.com@gmail.com>
2022-12-24 23:10:38 +0300

    task 1 fix 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,3 +1,26 @@
-﻿
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace Tasks
+{
+    public class Task1
+    {
+        public static void Main(string[] args)
+        {
+            string newCount;
+            switch (args[1])
+            {
+                case "rub":
+                    newCount = (double.Parse(args[0]) * 0.014).ToString("0.00");
+                    Console.WriteLine($"Your {args[0]}{args[1]} equal to {newCount}$");
+                    break;
+                case "$":
+                    newCount = (double.Parse(args[0]) * 70.25).ToString("0.00");
+                    Console.WriteLine($"Your {args[0]}{args[1]} equal to {newCount}rub");
+                    break;
+                default:
+                    Console.WriteLine("Undefined");
+                    break;
+            }
+        }
+    }
+}
