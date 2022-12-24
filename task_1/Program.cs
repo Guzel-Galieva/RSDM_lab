1a88e20e5b4f8d86ac84aede1db7db664c4ebbd7
guzel <guzel.vv.com@gmail.com>
2022-12-24 23:11:08 +0300

    task 1 fix 2

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,20 +6,28 @@ namespace Tasks
     {
         public static void Main(string[] args)
         {
-            string newCount;
-            switch (args[1])
+            try
             {
-                case "rub":
-                    newCount = (double.Parse(args[0]) * 0.014).ToString("0.00");
-                    Console.WriteLine($"Your {args[0]}{args[1]} equal to {newCount}$");
-                    break;
-                case "$":
-                    newCount = (double.Parse(args[0]) * 70.25).ToString("0.00");
-                    Console.WriteLine($"Your {args[0]}{args[1]} equal to {newCount}rub");
-                    break;
-                default:
-                    Console.WriteLine("Undefined");
-                    break;
+                string newCount;
+                switch (args[1])
+                {
+                    case "rub":
+                        newCount = (double.Parse(args[0]) * 0.014).ToString("0.00");
+                        Console.WriteLine($"Your {args[0]}{args[1]} equal to {newCount}$");
+                        break;
+                    case "$":
+                        newCount = (double.Parse(args[0]) * 70.25).ToString("0.00");
+                        Console.WriteLine($"Your {args[0]}{args[1]} equal to {newCount}rub");
+                        break;
+                    default:
+                        Console.WriteLine("Undefined");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
             }
         }
     }
